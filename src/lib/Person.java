44bfbfde59cf6ccee83495141a11d24c76ddc77b
Aditya MP <211wadityaw112@gmail.com>
2023-04-14 10:19:05 +0700

    membuat class Person

---
+++ src/lib/Person.java
@@ -0,0 +1,28 @@
+package lib;
+
+public class Person {
+    private int id;
+    private String name;
+
+    Person(String name, int id) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
