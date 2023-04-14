e8c7316f18502cd243331f84726b790a4d2db5cd
Aditya MP <211wadityaw112@gmail.com>
2023-04-14 13:29:38 +0700

    menghapus duplicate code

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -15,9 +15,10 @@ public class TaxFunction {
 	 */
 	
 	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isSingle, int numberOfChildren) {
 		
 		int tax = 0;
+		int nonTaxable  = 54000000;
 		
 		if (numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
@@ -27,18 +28,17 @@ public class TaxFunction {
 			numberOfChildren = 3;
 		}
 		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+		if (!isSingle) {
+			nonTaxable  = nonTaxable  + 4500000 + (numberOfChildren * 1500000);
 		}
+
+		tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - nonTaxable ));
 		
 		if (tax < 0) {
 			return 0;
-		}else {
-			return tax;
 		}
-			 
+		
+		return tax;
 	}
 	
 }
